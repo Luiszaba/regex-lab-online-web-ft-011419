abe15898856f65dcbe526db8f8a3c43e2624b9ce
Luis Zabala <luis.zabala076@gmail.com>
2019-01-25 23:30:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,6 +2,8 @@ def starts_with_a_vowel?(word)
 word.match(/^[aeiouAEIOU]\w+/) ? true : false 
 end
 
+
+
 def words_starting_with_un_and_ending_with_ing(text)
 text.scan(/\bun\w*ing\b/)
 end
@@ -14,6 +16,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 text.match(/^[A-Z].+[\.!?]$/) ? true:false
 end
 
+# Is the first (/^...) of word of the text capitalized?
+
 def valid_phone_number?(phone)
 phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end
@@ -23,6 +27,6 @@ end
 # -)(match 3 digits equal to 9) (match 4 digits equal to 9) => 
 # (###),(###)-(###) or (match 7 digits equal to 0-9)?   
 # If these conditions are met, you have a return value of true  
-# The digits given are a valid telephone number
+#  - The digits given are a valid telephone number
 # If the conditions are not met, return false
-# The digits given are not a valid telephone number
+#  - The digits given are not a valid telephone number
